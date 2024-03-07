cf7938487a68206eba50985345b5f64fecb6daa9
Eren <mehmeterenballi@gmail.com>
2024-03-08 00:06:53 +0300

    z index for queen and comment deletion

--- Assets/Scripts/Game/ChessGameManager.cs
+++ Assets/Scripts/Game/ChessGameManager.cs
@@ -78,16 +78,12 @@ public class ChessGameManager : MonoBehaviour
                     Debug.Log(hit.collider.gameObject.name);
                     if (hit.collider.TryGetComponent<ChessSquare>(out var chessSquareComponent))
                     {
-                        //int hitRow = chessSquareComponent.row;
-                        //int hitCol = chessSquareComponent.col;
 
-                        //Vector2 spawnPos = chessboardSquares[hitRow, hitCol].transform.position;
-                        //Debug.Log("Row: " + hitRow + ", Col: " + hitCol);
-                        //Debug.Log("spawnPos.x: " + spawnPos.x + ", spawnPos.y: " + spawnPos.y);
                         GameObject queen = Instantiate(queenPrefab, chessSquareComponent.gameObject.transform);
                         queen.transform.SetParent(canvas.transform, false);
                         queen.transform.SetParent(chessSquareComponent.gameObject.transform);
-                        queen.transform.localPosition = Vector3.zero;   
+                        //Vector3 squareCenter = chessSquareComponent.gameObject.transform.position;
+                        queen.transform.localPosition = new(0, 0, -2);
 
                     }
                     else
@@ -102,8 +98,6 @@ public class ChessGameManager : MonoBehaviour
     private void InitializeChessboardSquares()
     {
         // Initialize the chessboardSquares array
-        //chessboardSquares = new GameObject[rows, columns];
-
         chessboardSquares = new GameObject[rows, columns];
 
         // Populate the array with logical representations of the chessboard squares
@@ -117,7 +111,7 @@ public class ChessGameManager : MonoBehaviour
                 // Create a logical representation of the chessboard square
                 chessboardSquares[row, col] = new GameObject("ChessboardSquare_" + row + "_" + col);
                 chessboardSquares[row, col].transform.position = new Vector3(xPosition, yPosition, 0f);
-                //chessboardSquares[row, col].tag = "ChessboardSquare_" + row + "_" + col;
+
                 chessboardSquares[row, col].AddComponent<BoxCollider2D>().isTrigger = true;
                 // Set the Canvas as the parent of the chessboard square
                 chessboardSquares[row, col].transform.SetParent(canvas.transform, false);
@@ -133,86 +127,4 @@ public class ChessGameManager : MonoBehaviour
         }
     }
 
-    void OnChessSquareClicked(Vector2 queenSpawnPos)
-    {
-        // Get the clicked chessboard square position
-        //Vector3 clickedSquarePosition = GetChessSquareCenter(xPos, yPos);
-
-        // Instantiate the queen prefab at the clicked position
-        GameObject queen = Instantiate(queenPrefab, new(queenSpawnPos.x, queenSpawnPos.y, -2), Quaternion.identity);
-        queen.transform.SetParent(canvas.transform, false);
-    }
-
-    //Vector3 GetChessSquareCenter(float xPos, float yPos)
-    //{
-    //    // Calculate the position with an offset
-    //    //float offsetX = 30f;
-    //    //float offsetY = 86f;
-
-    //    Vector3 position = new(xPos * squareSize - squareSize / 2, yPos * squareSize - squareSize / 2, -2);
-
-    //    return position;
-    //}
-
-    Vector2 FindClosestCenter(Vector2 target, Vector2[,] centers)
-    {
-        Vector2 closestCenter = centers[0, 0];
-        float minDistance = Vector2.Distance(target, closestCenter);
-
-        for (int i = 0; i < centers.GetLength(0); i++)
-        {
-            for (int j = 0; j < centers.GetLength(1); j++)
-            {
-                Vector2 center = centers[i, j];
-                float distance = Vector2.Distance(target, center);
-
-                if (distance < minDistance)
-                {
-                    minDistance = distance;
-                    closestCenter = center;
-                }
-            }
-        }
-
-        return closestCenter;
-    }
-
-    Vector2 QueenSpawnPos(Vector2 clickedPosition)
-    {
-        // Calculate the logical indices of the clicked chessboard square
-        //int row = Mathf.FloorToInt(clickedPosition.y);
-        //int col = Mathf.FloorToInt(clickedPosition.x);
-
-        float squareBorderWidth = boardWidth / 8;
-        float squareBorderHeight = boardHeight / 8;
-
-        float firstXCenter = bottomLeft.x + squareBorderWidth / 2;
-        float firstYCenter = bottomLeft.y + squareBorderHeight / 2;
-
-        float[] squareXCenterPoints = new float[8];
-        float[] squareYCenterPoints = new float[8];
-
-        for (int k = 0; k < 8; k++)
-        {
-            squareXCenterPoints[k] = firstXCenter + squareBorderWidth * k;
-            squareYCenterPoints[k] = firstYCenter + squareBorderHeight * k;
-        }
-
-        Vector2[,] allCenterPoints = new Vector2[8, 8];
-
-        for (int i = 0; i < 8; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                allCenterPoints[i, j] = new Vector2(squareXCenterPoints[i], squareYCenterPoints[j]);
-                Debug.Log(allCenterPoints[i, j]);
-            }
-        }
-
-        // En yakýn merkezi bulma
-        Vector2 closestCenter = FindClosestCenter(clickedPosition, allCenterPoints);
-
-        //float[] indices = { col, row };
-        return closestCenter;
-    }
 }
